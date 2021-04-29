7158aba9c645eb75228c07a963aae52f5c0a6380
heta bhatt <heta.bhatt09@gmail.com>
2021-04-29 17:45:06 +1200

    Add project files.

---
+++ Console3/Program.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), "2.25");
+            _webDriver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+
+        [Test]
+        public void Test()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(_webDriver.Title.Contains("Google"));
+        }
+    }
+}
